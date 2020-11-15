310fb1045c0ee609ab5d8948d2b95a6094b67242
Nick Morgan <nick@social-sync.com>
2020-11-15 11:39:43 +0000

    Took me 10 decades to figure it out... but the icons now change colour when clicked!
    Signed-off-by: Nick Morgan <nick@social-sync.com>

--- src/Component/ImageResults/ImageResults.component.jsx
+++ src/Component/ImageResults/ImageResults.component.jsx
@@ -1,23 +1,15 @@
-import React, {useContext, useState, useEffect} from 'react';
+import React, {useContext} from 'react';
 import styles from './ImageResults.module.css';
 import { GridList, GridListTile, Card, CardMedia, IconButton } from "@material-ui/core";
 import FavoriteIcon from '@material-ui/icons/FavoriteBorder';
-import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
 import LinkIcon from '@material-ui/icons/Link';
 import {APIContext, FavouritesContext} from "../../context/GlobalContext";
 
 const ImageResults = () => {
     const [api, getAPI ] = useContext(APIContext);
     const [favourites, setFavourites] = useContext(FavouritesContext);
-    const [favouriteClick, setFavouriteClick] = useState(false);
     const data = api.hits;
     
-    useEffect(async () => {
-        let savedFavorite = await localStorage.getItem('__Fav');
-        if(savedFavorite) {
-            setFavouriteClick(savedFavorite);
-        }
-    }, [])
     
     const colsDef = () => {
         let finalCols = 5;
@@ -38,26 +30,29 @@ const ImageResults = () => {
         <div className={styles.container}>
             <GridList className={styles.gridList} cols={colsDef()} cellHeight={300} spacing={5}>
                 {data != null ? (
-                    data.map(item => (
-                                <GridListTile key={item.id} cols={1} >
-                                    <Card className={styles.card}>
-                                        <CardMedia
-                                            className={styles.image}
-                                            src={item.largeImageURL}
-                                            image={item.largeImageURL}
-                                        />
-                                        <IconButton className={styles.icon} onClick={() => handleIconClick(item.largeImageURL)}>
-                                            {favouriteClick == false
-                                                ? <FavoriteBorderOutlinedIcon key="__Fav'"/>
-                                                :   <FavoriteIcon />
-                                            }
-                                        </IconButton>
-                                        <IconButton onClick={() => window.open(item.largeImageURL)}>
-                                            <LinkIcon />
-                                        </IconButton>
-                                    </Card>
-                                </GridListTile>
-                    ))) : ' '}
+                    data.map(item => {
+                        return (
+                            <GridListTile key={item.id} cols={1} >
+                                <Card className={styles.card}>
+                                    <CardMedia
+                                        className={styles.image}
+                                        src={item.largeImageURL}
+                                        image={item.largeImageURL}
+                                    />
+                                    <IconButton
+                                        className={styles.icon}
+                                        onClick={() => handleIconClick(item.largeImageURL)}
+                                        color={favourites.includes(item.largeImageURL) ? "secondary" : "inherit"}
+                                    >
+                                        <FavoriteIcon />
+                                    </IconButton>
+                                    <IconButton onClick={() => window.open(item.largeImageURL)}>
+                                        <LinkIcon />
+                                    </IconButton>
+                                </Card>
+                            </GridListTile>
+                            )
+                            })) : ' '}
             </GridList>
         </div>
 
