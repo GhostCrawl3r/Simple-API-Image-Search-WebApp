de709691c32f676393dae487410689d6030d48b8
Nick Morgan <nick@social-sync.com>
2020-11-15 01:08:21 +0000

    Removed the card component. Changed the ImageResults.component.jsx to show response depending on screensize. I also styled the elements using css and also set the images to contain.

--- src/Component/ImageResults/ImageResults.component.jsx
+++ src/Component/ImageResults/ImageResults.component.jsx
@@ -1,42 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './ImageResults.module.css';
-import { makeStyles } from '@material-ui/core/styles';
 import {GridList, GridListTile} from "@material-ui/core";
 
-import Card from "../Card/Card.component";
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        justifyContent: 'space-around',
-    },
-    gridList: {
-        width: 1500,
-        height: 700,
-    },
-}));
-
 const ImageResults = ({results}) => {
-    const classes = useStyles();
     const data = results.hits;
     
     const colsDef = () => {
         let finalCols = 5;
-        
-        if(window.innerWidth < 770) {
+        if (window.innerWidth < 770) {
             finalCols = 1;
         }
-        
         return finalCols;
     }
     
     return (
-        <div className={classes.root}>
-            <GridList className={classes.gridList} cols={colsDef()} cellHeight={500} spacing={5}>
+        <div className={styles.container}>
+            <GridList className={styles.gridList} cols={colsDef()} cellHeight={300} spacing={5}>
                 {data != null ? (
                     data.map(item => (
                         <GridListTile key={item.id} cols={1} >
-                            <img src={item.largeImageURL} alt={item.user} onClick={() => window.open(item.largeImageURL)}/>
+                            <img src={item.largeImageURL} alt={item.user} onClick={() => window.open(item.largeImageURL)} className={styles.image}/>
                         </GridListTile>
                     ))) : ' '}
             </GridList>
