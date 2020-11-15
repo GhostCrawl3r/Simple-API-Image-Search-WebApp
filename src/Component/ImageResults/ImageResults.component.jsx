e1a07023898e7f2b4a11daeb137cc8cac117cf3a
Nick Morgan <nick@social-sync.com>
2020-11-15 02:29:59 +0000

    Styled the images in card. Added a favourite button and a link button. I figure it's more intuitive to click a link button for source as opposed to the image itself.

--- src/Component/ImageResults/ImageResults.component.jsx
+++ src/Component/ImageResults/ImageResults.component.jsx
@@ -1,8 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import styles from './ImageResults.module.css';
-import {GridList, GridListTile} from "@material-ui/core";
+import {GridList, GridListTile, Card, CardMedia, IconButton, Grid, Icon} from "@material-ui/core";
+import FavoriteIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
+import LinkIcon from '@material-ui/icons/Link';
 
 const ImageResults = ({results}) => {
+    const [click, setClick] = useState(false);
     const data = results.hits;
     
     const colsDef = () => {
@@ -13,14 +17,33 @@ const ImageResults = ({results}) => {
         return finalCols;
     }
     
+    const handleIconClick = () => {
+        setClick(!click);
+    }
+    
     return (
         <div className={styles.container}>
             <GridList className={styles.gridList} cols={colsDef()} cellHeight={300} spacing={5}>
                 {data != null ? (
                     data.map(item => (
-                        <GridListTile key={item.id} cols={1} >
-                            <img src={item.largeImageURL} alt={item.user} onClick={() => window.open(item.largeImageURL)} className={styles.image}/>
-                        </GridListTile>
+                                <GridListTile key={item.id} cols={1} >
+                                    <Card className={styles.card}>
+                                        <CardMedia
+                                            className={styles.image}
+                                            src={item.largeImageURL}
+                                            image={item.largeImageURL}
+                                        />
+                                        <IconButton className={styles.icon} onClick={handleIconClick}>
+                                            {!click
+                                                ? <FavoriteBorderOutlinedIcon />
+                                                :   <FavoriteIcon />
+                                            }
+                                        </IconButton>
+                                        <IconButton onClick={() => window.open(item.largeImageURL)}>
+                                            <LinkIcon />
+                                        </IconButton>
+                                    </Card>
+                                </GridListTile>
                     ))) : ' '}
             </GridList>
         </div>
